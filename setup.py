dc7fc432362b7b61109fadfa22eb8645c9afa3d7
Nerozac Air <Li.J.W.adrian421@hotmail.com>
2025-02-25 20:58:32 +0800

    2025年02月25日 Tuesday HKT 20:58:30 : 提交代码更新

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wixio",
+    version="0.0.1",
+    description="A Wide x file IO utility library for Python.",
+    author="Jiawei Li",
+    author_email="=Li.J.W.adrian421@hotmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.8",
+)
